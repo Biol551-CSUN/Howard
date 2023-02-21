b1d4c7d7c3dc166fe7b11c2823a068ac810d500a
Kristen Howard <kristen.howard.739@my.csun.edu>
2023-02-21 14:31:37 -0800

    Created tibbles to practice joining functions in R

--- Week_05/Scripts/Joining_Script.R
+++ Week_05/Scripts/Joining_Script.R
@@ -45,22 +45,24 @@ FullData_left<- left_join(TPCData, EnviroData_wide) %>%
 ##################################################
 # Calculate mean and variance of all collected (TPC and environmental data by site)
 # EnviroData is already long. Convert TPC data to long
-TPCData_Long <- TPCData %>%
-              pivot_longer(cols = E:Topt,
-                           names_to  = "Variables",
-                           values_to = "Values") %>%
-              arrange(site.letter)
-View(TPCData_Long)
-#Mean and variance of TPC
-TPCData_Long %>%
-    group_by(Variables, site.letter) %>% #Group by variables and site letter
-    summarise(Param_means = mean(Values, na.rm = TRUE),# Get mean
-              Param_vars = mean(Values, na.rm = TRUE)) #get variance
-#Mean and variance of EnviroData
-EnviroData %>%
-  group_by(parameter.measured, site.letter) %>% #Group by variables and site letter
-  summarise(Param_means = mean(values, na.rm = TRUE),# Get mean
-            Param_vars = mean(values, na.rm = TRUE)) #get variance
+
+# ###THIS IS ALL WRONG!!!!!!!
+# TPCData_Long <- TPCData %>%
+#               pivot_longer(cols = E:Topt,
+#                            names_to  = "Variables",
+#                            values_to = "Values") %>%
+#               arrange(site.letter)
+# View(TPCData_Long)
+# #Mean and variance of TPC
+# TPCData_Long %>%
+#     group_by(Variables, site.letter) %>% #Group by variables and site letter
+#     summarise(Param_means = mean(Values, na.rm = TRUE),# Get mean
+#               Param_vars = mean(Values, na.rm = TRUE)) #get variance
+# #Mean and variance of EnviroData
+# EnviroData %>%
+#   group_by(parameter.measured, site.letter) %>% #Group by variables and site letter
+#   summarise(Param_means = mean(values),# Get mean
+#             Param_vars = mean(values)) #get variance
 
 ####################################
 # Here is how you create tibble
@@ -74,16 +76,53 @@ T2
 ###################################
 #left_join v.s right_join
 left_join(T1, T2)  #This has T1 site.ID in it
-right_join(T1, T2) #This has T2 site.ID in it
+right_join(T1, T2) #This has T2 site.ID in it, the E is NA because T1 does not have that site
 
 #inner_join vs full_join
 inner_join(T1,T2) # Only keeps data that is complete 
-full_join(T1,T2) #Keeps everything and NAs
+full_join(T1,T2) #Keeps everything and NAs. This retains ALL the information. 
+                 #This is similar to merge
 
 #semi_join vs anti_join
 semi_join(T1, T2) #keeps all frows from first data set
-anti_join(T1, T2) #keeps just columns from first set
+anti_join(T1, T2) #keeps just columns from first set. Keeps coluns that are missing
 
 library(cowsay)
 say("hey sexy baby", by = "cat")
 ###############################################
+# Lab practice
+# Practice Joining
+#We have a scientist that collected protozoan area and numercial counts 
+#from different pitcher plants found at different sites. 
+#Practice your joining functions and create a tibble
+
+T3 <- tibble(Site.ID = c("A", "B", "C", "D"),
+             Protozoa.area = c(0.003, 0.523 ,23.0 ,0.0003))
+
+T4 <- tibble(Site.ID = c("A", "B", "C", "K") ,   
+            Protozoa.counts = c(34444, 22223, 3333, 32344)) 
+T3 #View the data you just created
+T4 #View the data you just created
+
+left_join(T3, T4) #See how there is an NA under P.counts? There is no Site D in T4
+right_join(T3, T4) #See how there is an NA under P.area. There is no Site K in T3
+inner_join(T3, T4) #Only keeps data that is complete from T3 and T4
+full_join(T3, T4) #Keeps all data including NAs in T3 & T4
+anti_join(T3,T4) #Dpes the opposite of semi join, keeps columns that are missing in T4
+anti_join(T4,T3) #See, now it keeps K
+semi_join(T3, T4) #Keeps the information from T3 that has a matching value at site in T4
+
+
+
+T5 <- tibble(Site.ID = c("A", "B", "C", "D"),
+             Pitcher.Plant.ID = c(23,67,23,45),
+             Protozoa.area = c(0.003, 0.523 ,23.0 ,0.0003))
+T5
+T6 <- tibble(Site.ID = c("A", "B", "C", "K") , 
+             Pitcher.Plant.ID = c(23,67,23,45),
+             Protozoa.counts = c(34444, 22223, 3333, 32344)) 
+T6
+
+left_join(T5,T6)
+right_join(T5,T6)
+full_join(T5,T6)
